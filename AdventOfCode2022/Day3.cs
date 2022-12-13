5bc7d67af4dbf4c63cbe317d1c2f42d1211aef37
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-12-13 14:24:35 +0300

    day 3 finished, a little bug is present in part 2 (works in example input though)

---
+++ AdventOfCode2022/Day3.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day3
+    {
+        static string[] DataReader()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Yetkin\OneDrive\Desktop\Projects\AdventOfCode2022\AdventOfCode2022\Day3.txt");
+
+            return lines;
+        }
+
+        static IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+
+        static int FindLetterIndex(char letter)
+        {
+            int index = 0;
+
+            if (char.IsLower(letter))
+            {
+                index = (int)letter % 32;
+            }
+            else
+            {
+                index = ((int)letter % 32) + 26;
+            }
+
+            return index;
+        }
+
+        public static void Part1()
+        {
+            string[] lines = DataReader();
+
+            int indexTotal = 0;
+
+            foreach (string line in lines)
+            {
+                string[] compartments = Split(line, line.Length / 2).ToArray();
+
+                var commonLetter = compartments[0].Intersect(compartments[1]);
+
+                foreach (var letter in commonLetter)
+                {
+                    indexTotal += FindLetterIndex(letter);
+                }
+            }
+
+            Console.WriteLine(indexTotal.ToString());
+        }
+
+        public static void Part2()
+        {
+            string[] lines = DataReader();
+            int indexTotal = 0;
+
+            IEnumerable<string[]> chunks = lines.Chunk(lines.Length / 2);
+
+            foreach (string[] bags in chunks)
+            {
+                string currentBag = bags[0];
+                char mostCommonLetter = " "[0];
+
+                foreach (string bag in bags)
+                {
+                    foreach (var letter in currentBag.Intersect(bag))
+                    {
+                        mostCommonLetter = letter;
+                    }
+
+                    currentBag = bag;
+                    indexTotal += FindLetterIndex(mostCommonLetter);
+                }
+
+                // Console.WriteLine("\n");
+                Console.WriteLine(indexTotal.ToString());
+            }
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,10 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2022;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Advent of Code!");
 
+Console.WriteLine("Day 1");
 Day1.Part1();
 Day1.Part2();
 
+Console.WriteLine("Day 2");
 Day2.Part1();
 Day2.Part2();
+
+Console.WriteLine("Day 3");
+Day3.Part1();
+Day3.Part2();
