a6fa9aa14642e92838b7d6bdee6224415374cf7b
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-12-27 14:25:37 +0300

    scenic score didn't work, debugging took so long so, a little help from the internet handled the implementation of part 2

--- AdventOfCode2022/Day8.cs
+++ AdventOfCode2022/Day8.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2022
 {
-    internal class Day8
+    static internal class Day8
     {
+        public static void GetGridMetadata(this ReadOnlySpan<char> input, out int rowSize, out int rowSkip)
+        {
+            // Row size == index of the first newline from the start of the input
+            rowSize = input.IndexOfAny('\r', '\n');
+            if (rowSize < 1) throw new ArgumentException("First line does not have any content", nameof(input));
+
+            // Newline size == distance between the first newline character and the next non-newline character
+            if (input[rowSize + 1] is not '\r' or '\n') rowSkip = rowSize + 1;
+            else if (input[rowSize + 2] is not '\r' or '\n') rowSkip = rowSize + 2;
+            else throw new ArgumentException("Newline is not \\r\\n (CRLF) or \\n (LF)", nameof(input));
+        }
+
         static string[] DataReader()
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Yetkin\OneDrive\Desktop\Projects\AdventOfCode2022\AdventOfCode2022\Day8.txt");
@@ -267,11 +279,95 @@ namespace AdventOfCode2022
             }
         }
 
+        private static int ComputeScenicValue(int row, int col, ReadOnlySpan<char> input, int rowSkip, int gridSize)
+        {
+            var tree = ReadTree(row, col, input, rowSkip);
+
+            // MULTIPLY, not add!!
+            return
+                // Top
+                CountVisibleTreesFrom(tree, row, col, 0, -1, input, rowSkip, gridSize) *
+
+                // Right
+                CountVisibleTreesFrom(tree, row, col, 1, 0, input, rowSkip, gridSize) *
+
+                // Bottom
+                CountVisibleTreesFrom(tree, row, col, 0, 1, input, rowSkip, gridSize) *
+
+                // Left
+                CountVisibleTreesFrom(tree, row, col, -1, 0, input, rowSkip, gridSize);
+        }
+
+        static char ReadTree(int row, int col, ReadOnlySpan<char> input, int rowSkip)
+        {
+            var index = (row * rowSkip) + col;
+            return input[index];
+        }
+
+        private static int CountVisibleTreesFrom(char fromTree, int row, int col, int rowIncrement, int colIncrement, ReadOnlySpan<char> input, int rowSkip, int gridSize)
+        {
+            var count = 0;
+
+            do
+            {
+                row += rowIncrement;
+                col += colIncrement;
+
+                // Need to break early to avoid counting outside the edges
+                if (row < 0 || row >= gridSize || col < 0 || col >= gridSize)
+                {
+                    break;
+                }
+
+                count++;
+            } while (ReadTree(row, col, input, rowSkip) < fromTree);
+
+            return count;
+        }
+
+        static IEnumerable<(int dx, int dy)> EnumDirections()
+        {
+            yield return (0, -1);
+            yield return (-1, 0);
+            yield return (0, 1);
+            yield return (1, 0);
+        }
+        static IEnumerable<(int x, int y)> EnumPositions((int x, int y) start, (int x, int y) dir, int size)
+            => from i in Enumerable.Range(0, size)
+            let x = start.x + (i * dir.x)
+            let y = start.y + (i * dir.y)
+            where x >= 0 && x < size && y >= 0 && y < size
+            select (x, y);
+
+        static int GetViewDistance(int[][] map, (int x, int y) start, (int x, int y) dir, int size)
+        {
+            var height = map[start.y][start.x];
+            var count = 0;
+            foreach (var (x, y) in EnumPositions(start, dir, size).Skip(1))
+            {
+                ++count;
+                if (map[y][x] >= height)
+                    break;
+            }
+
+            return count;
+        }
+
+        static int GetScenicScore(int[][] map, (int x, int y) start, int size) 
+            => EnumDirections().Select(d => GetViewDistance(map, start, d, size))
+            .Aggregate(1, (a, n) => a * n);
+
+
         public static void Part2()
         {
             string[] data = DataReader();
             int[,] treeMap = MapGenerator(data);
 
+            var map = System.IO.File.ReadLines(@"C:\Users\Yetkin\OneDrive\Desktop\Projects\AdventOfCode2022\AdventOfCode2022\Day8.txt")
+                .Select(line => line.Select(ch => ch - '0').ToArray())
+                .ToArray();
+
+            /*
             int scenicScore = 1;
             int maxScenicScore = 0;
 
@@ -292,8 +388,40 @@ namespace AdventOfCode2022
                     }
                 }
             }
+            */
 
-            Console.WriteLine(maxScenicScore);
+            /*
+            var maxScenic = 0;
+
+            string inputFile = "C:\\Users\\Yetkin\\OneDrive\\Desktop\\Projects\\AdventOfCode2022\\AdventOfCode2022\\Day8.txt";
+            var input = inputFile.AsSpan().TrimEnd();
+
+            // Map the input to be indexed as a grid
+            input.GetGridMetadata(out var gridSize, out var rowSkip);
+
+            // Brute force this thing:    
+            // Check each tree against each other tree in O(n^2) worst-case time!
+            for (var row = 0; row < treeMap.GetLength(0); row++)
+            {
+                for (var col = 0; col < treeMap.GetLength(1); col++)
+                {
+                    var scenicValue = ComputeScenicValue(row, col, input, rowSkip, gridSize);
+                    if (scenicValue > maxScenic)
+                    {
+                        maxScenic = scenicValue;
+                    }
+                }
+            }
+
+            Console.WriteLine(maxScenic);
+            */
+
+            var size = map.Length;
+            var viewDistances = from x in Enumerable.Range(0, size)
+                                from y in Enumerable.Range(0, size)
+                                select GetScenicScore(map, (x, y), size);
+
+            Console.WriteLine(viewDistances.Max());
         }
     }
 }
